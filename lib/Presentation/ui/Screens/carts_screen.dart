fa2becee9e54e08565e6d0c2fc0e016fa7612ae1
yasin <yasinmiapolash@gmail.com>
2024-02-11 23:46:37 +0600

    next

--- lib/Presentation/ui/Screens/carts_screen.dart
+++ lib/Presentation/ui/Screens/carts_screen.dart
@@ -1,4 +1,5 @@
 import 'package:crafty_bay/Presentation/state_holder/main_bottom_nav_controller.dart';
+import 'package:crafty_bay/Presentation/ui/Utility/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,21 +13,68 @@ class CartsScreen extends StatefulWidget {
 class _CartsScreenState extends State<CartsScreen> {
   @override
   Widget build(BuildContext context) {
-    return  PopScope(
+    return PopScope(
       canPop: false,
-      onPopInvoked: (_){
+      onPopInvoked: (_) {
         Get.find<MainBottomNavController>().backToHome();
-
       },
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Cart'),
-          leading:  IconButton(
-            onPressed: (){
+          leading: IconButton(
+            onPressed: () {
               Get.find<MainBottomNavController>().backToHome();
             },
             icon: const Icon(Icons.arrow_back_ios),
           ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Column(),
+            ),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor.withOpacity(0.15),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Total Price',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black45),
+                      ),
+                      Text(
+                        '10000',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.primaryColor),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('CheckOut'),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
