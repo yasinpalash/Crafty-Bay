87358183df9ab1a7e0ba949c1543372311aed1c8
yasin <yasinmiapolash@gmail.com>
2024-02-11 21:53:14 +0600

    next

--- lib/Presentation/ui/Screens/carts_screen.dart
+++ lib/Presentation/ui/Screens/carts_screen.dart
@@ -1,4 +1,6 @@
+import 'package:crafty_bay/Presentation/state_holder/main_bottom_nav_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartsScreen extends StatefulWidget {
   const CartsScreen({super.key});
@@ -10,6 +12,23 @@ class CartsScreen extends StatefulWidget {
 class _CartsScreenState extends State<CartsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  PopScope(
+      canPop: false,
+      onPopInvoked: (_){
+        Get.find<MainBottomNavController>().backToHome();
+
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cart'),
+          leading:  IconButton(
+            onPressed: (){
+              Get.find<MainBottomNavController>().backToHome();
+            },
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
+        ),
+      ),
+    );
   }
 }
