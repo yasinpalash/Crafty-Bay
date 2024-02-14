c2dcd37b0b5f2f1536f325558c811d141ea966eb
yasin <yasinmiapolash@gmail.com>
2024-02-15 00:41:37 +0600

    next

--- lib/Presentation/ui/Screens/product_details_screen.dart
+++ lib/Presentation/ui/Screens/product_details_screen.dart
@@ -18,8 +18,10 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
     Colors.amber,
     Colors.lightGreen,
     Colors.green,
-    Colors.white54
+    Colors.red
   ];
+  Color _selectedColor = Colors.black;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,6 +122,12 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                         style: TextStyle(
                             fontSize: 16, fontWeight: FontWeight.w500),
                       ),
+                      ColorSelector(
+                        colors: colors,
+                        onChange: (selectedColor) {
+                          _selectedColor = selectedColor;
+                        },
+                      )
                     ],
                   ),
                 )
@@ -173,6 +181,59 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           )
         ],
       ),
+    );
+  }
+}
+
+class ColorSelector extends StatefulWidget {
+  const ColorSelector(
+      {super.key, required this.colors, required this.onChange});
+  final List<Color> colors;
+  final Function(Color) onChange;
+
+  @override
+  State<ColorSelector> createState() => _ColorSelectorState();
+}
+
+class _ColorSelectorState extends State<ColorSelector> {
+  late Color _selectedColor;
+  @override
+  void initState() {
+    super.initState();
+    _selectedColor = widget.colors.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: widget.colors
+          .map(
+            (c) => InkWell(
+              onTap: () {
+                widget.onChange;
+                _selectedColor = c;
+                if (mounted) {
+                  setState(() {});
+                }
+              },
+              borderRadius: BorderRadius.circular(15),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: c,
+                  child: _selectedColor == c
+                      ? const Icon(
+                          Icons.done,
+                          color: Colors.white,
+                        )
+                      : null,
+                ),
+              ),
+            ),
+          )
+          .toList(),
     );
   }
 }
