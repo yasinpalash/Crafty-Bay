89deb0e0611e2c8f1c2af216ade506267b760704
yasin <yasinmiapolash@gmail.com>
2024-03-05 14:43:29 +0600

    next

--- lib/Presentation/state_holder/complete_profile_controller.dart
+++ lib/Presentation/state_holder/complete_profile_controller.dart
@@ -1,22 +1,44 @@
 import 'package:crafty_bay/data/Utility/urls.dart';
+import 'package:crafty_bay/data/models/profile.dart';
 import 'package:crafty_bay/data/services/network_caller.dart';
 import 'package:get/get.dart';
 
-class CompleteProfileController extends GetxController{
+class CompleteProfileController extends GetxController {
+  bool _inProgress = false;
+  bool get inProgress => _inProgress;
+  Profile _profile = Profile();
+  Profile get profile => _profile;
+  String _errorMessage = '';
+  String get errorMessage => _errorMessage;
 
-  bool _inProgress=false;
-
-  bool get inProgress=> _inProgress;
-
-  Future<bool> createProfile(String token) async{
-
-    _inProgress=true;
+  Future<bool> createProfileDate(
+    String token,
+    String firstName,
+    String lastName,
+    String mobile,
+    String city,
+    String shippingAddress,
+  ) async {
+    _inProgress = true;
     update();
-    final response= await NetworkCaller().postRequest(Urls)
-
-
-
-
+    Map<String, dynamic> inputParams = {
+      "firstName": firstName,
+      "lastName": lastName,
+      "mobile": mobile,
+      "city": city,
+      "shippingAddress": shippingAddress,
+    };
+    final response = await NetworkCaller()
+        .postRequest(Urls.createProfile, token: token, body: inputParams);
+    _inProgress = false;
+    if (response.isSuccess) {
+      _profile = Profile.fromJson(response.responseData['data']);
+      update();
+      return true;
+    } else {
+      _errorMessage = response.errorMessage;
+      update();
+      return false;
+    }
   }
-
 }
--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -37,7 +37,7 @@ class NetworkCaller {
   }
 
   Future<ResponseData> postRequest(String url,
-      {Map<String, dynamic>? body}) async {
+      {Map<String, dynamic>? body,String? token}) async {
     log(url);
     log(body.toString());
     final Response response =
